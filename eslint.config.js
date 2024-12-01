f107bcb663c5ba4457ea4f59bd9055bad791d894
Mitsuyuki Shiiba <bufferings@gmail.com>
2024-12-01 14:18:18 +0900

    Finish clean up

--- eslint.config.js
+++ eslint.config.js
@@ -2,24 +2,8 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-// import { FlatCompat } from "@eslint/eslintrc";
-import path from "path";
-import { fileURLToPath } from "url";
-
-// import myPlugin from "./my-plugin/index.js";
 import { fixupPluginRules } from "@eslint/compat";
 import eslintPluginNeverthrow from "eslint-plugin-neverthrow";
-
-// mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// const compat = new FlatCompat({
-//   baseDirectory: __dirname,                  // optional; default: process.cwd()
-//   resolvePluginsRelativeTo: __dirname,       // optional
-//   recommendedConfig: pluginJs.configs.recommended, // optional unless you're using "eslint:recommended"
-//   allConfig: pluginJs.configs.all,                 // optional unless you're using "eslint:all"
-// });
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -30,44 +14,11 @@ export default [
   {
     languageOptions: {
       parserOptions: {
+        // because this project uses TS 5.7 which isn't yet officially supported by ESLint
         warnOnUnsupportedTypeScriptVersion: false,
       }
     }
   },
-
-  // ...compat.config({
-  //   plugins: ['neverthrow'],
-  //   rules: {
-  //     'neverthrow/must-use-result': 'error',
-  //   },
-  //   parser: '@typescript-eslint/parser',
-  //   parserOptions: {
-  //     ecmaVersion: 2021,
-  //     sourceType: 'module',
-  //     project: ['./tsconfig.json'],
-  //     tsconfigRootDir: __dirname,
-  //   },
-  // }),
-
-  // ローカルプラグインを使う場合
-  // {
-  //   plugins: {
-  //     myPlugin: fixupPluginRules(myPlugin),
-  //   },
-  //   languageOptions: {
-  //     parser: tseslint.parser,
-  //     parserOptions: {
-  //       ecmaVersion: 2021,
-  //       sourceType: 'module',
-  //       project: ['./tsconfig.json'],
-  //       tsconfigRootDir: __dirname,
-  //     }
-  //   },
-  //   rules: {
-  //     'myPlugin/must-use-result': 'error',
-  //   },
-  // },
-
   {
     plugins: {
       eslintPluginNeverthrow: fixupPluginRules(eslintPluginNeverthrow),
@@ -78,7 +29,8 @@ export default [
         ecmaVersion: 2021,
         sourceType: 'module',
         project: ['./tsconfig.json'],
-        tsconfigRootDir: __dirname,
+        // We can use import.meta.dirname with Node.js >= 21.2.0
+        tsconfigRootDir: import.meta.dirname,
       }
     },
     rules: {
